8dd7a3639a90762a034025dd24a10c58eaa7a880
Sam-Watling <samwatling123@hotmail.co.uk>
2022-10-12 12:49:22 +0100

    cars added

---
+++ Assets/Scripts/Traffic/Traffic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traffic : MonoBehaviour
+{
+    [Serializedfield] private const float maxVehicles = 10;
+    private Vehicle[maxVehicles] vehicles; 
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
