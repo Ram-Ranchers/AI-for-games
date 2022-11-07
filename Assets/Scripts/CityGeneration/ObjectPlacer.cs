e60b7034c17781cb80bfb3449f8fbf24059c3d4f
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-11-07 17:46:57 +0000

    Buildings now sit on top of terrain as well as try match the angle of a slope if on one

---
+++ Assets/Scripts/CityGeneration/ObjectPlacer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    void Start()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = hit.point + 0.5f * hit.normal;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+        else
+        {
+            ray = new Ray(transform.position, transform.up);
+            if (Physics.Raycast(ray, out hit))
+            {
+                transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            }
+        }
+    }
+}
