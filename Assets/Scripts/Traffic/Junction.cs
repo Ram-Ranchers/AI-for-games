811a92656bcb0caf9ab2d306fb7ec1bca7ed6fc7
Sam Watling <samwatling123@hotmail.co.uk>
2022-11-28 13:28:34 +0000

    junctions be needing some vehicles for the testing of things

--- Assets/Scripts/Traffic/Junction.cs
+++ Assets/Scripts/Traffic/Junction.cs
@@ -25,19 +25,36 @@ public class Junction : MonoBehaviour
     private List<Vehicle> vehiclesInJunction;
     private List<bool> activeRoads;
 
+    [SerializeField] private float cycleTime = 10.0f;
+    private float cycleLast;
+
+    private int lastActiveRoad;
+
     private void Start()
     {
-        
+        cycleLast = Time.time;
     }
 
-    //transfer out
+    private void Update()
+    {
+        //on some trigger call pathgeneration, need to make a tool or soemthing?
 
+        if(cycleLast+cycleTime>Time.time)
+        {
+            lightCycle();
+        }
 
-    //transfer in
-
+    }
 
     //cycle traffic control
-
+    void lightCycle()
+    {
+        //TODO Pedestrian stuff?
+        for (int i = 0; i < activeRoads.Count; i++)
+        {
+            activeRoads[i] = !activeRoads[i];
+        }
+    }
 
 
     //generate pathways
@@ -53,7 +70,7 @@ public class Junction : MonoBehaviour
                         continue;
                     for (int l = 0; l < connectedRoads[k].lanesRight; l++)
                     {
-
+                        //gen spline path using vehicle heading 
                     }
                 }
             }
