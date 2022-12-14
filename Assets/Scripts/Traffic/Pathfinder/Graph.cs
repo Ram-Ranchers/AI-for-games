b4cd46da52e1868d206bf7869dad4f6fe21a09c3
Sam Watling <samwatling123@hotmail.co.uk>
2022-12-14 10:47:41 +0000

    mm

--- Assets/Scripts/Traffic/Pathfinder/Graph.cs
+++ Assets/Scripts/Traffic/Pathfinder/Graph.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
+    public Dictionary<GraphNode, GraphNode[]> edges = new Dictionary<GraphNode, GraphNode[]>();
+
+    public GraphNode[] Neighbors(GraphNode id)
+    {
+        return edges[id];
+    }
+
     // Start is called before the first frame update
     void Start()
     {
